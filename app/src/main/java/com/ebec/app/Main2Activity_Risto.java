5105ee66034afaffad9582c500ba745f2da9e9ef
Risto Põldsalu <risto.pold@gmail.com>
2015-11-05 21:25:16 +0200

    balala

--- app/src/main/java/com/ebec/app/Main2Activity_Risto.java
+++ app/src/main/java/com/ebec/app/Main2Activity_Risto.java
@@ -3,11 +3,11 @@ package com.ebec.app;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Main2Activity extends AppCompatActivity {
+public class Main2Activity_Risto extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main2_Risto);
     }
 }
